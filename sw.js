cc06947b8f75d2cd4593c1773685086f399df733
nathan87nt-ctrl <nathan87.nt@gmail.com>
2026-09-03 14:14:25 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fuellog-v17";
+const CACHE = "fuellog-v18";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
